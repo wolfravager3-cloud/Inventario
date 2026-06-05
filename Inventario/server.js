cede9c58eca4d2993174fd794fde944db87b6091
Cris <wolfravager3@gmail.com>
2026-06-04 22:04:01 -0600

    Mostrar index.html en Render

--- Inventario/server.js
+++ Inventario/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Archivos estáticos
+app.use(express.static(__dirname));
+
+// API
 app.use('/productos', require('./routes/productos'));
+
+// Página principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 
